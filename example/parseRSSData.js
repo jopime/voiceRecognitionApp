f8f02bcc727a10674d59cb18505e4d7bf1352359
SergioPadilla <splsergiopadilla@gmail.com>
2016-11-19 18:54:11 +0100

    refactor

--- example/parseRSSData.js
+++ example/parseRSSData.js
@@ -1,42 +1,30 @@
+// this counts how many headlines are available for a team
+function getCount(d) {
+	var x=d.getElementsByTagName("item");
+	// invoked using "getCount(dataName)"/>
 
-	// this counts how many headlines are available for a team
-	function getCount(d)
-	{
-		var x=d.getElementsByTagName("item");
-    	// invoked using "getCount(dataName)"/>
+	return x.length;
+}
 
-		return x.length;
+
+// grab the values of node 'n', and append the values to an array
+function assignArray(d, n, r) {
+    var j=(d.getElementsByTagName(r).length + 2);
+	var NBAarray = new Array();
+
+	for(var i = 0; i < j; i++) {
+        // since its possible that a title or description entry from the RSS feed
+        // will be empty, or have 'bad' characters, we need to do some error handling
+
+        try {
+            NBAarray[i] = d.getElementsByTagName(n).item(i).firstChild.data;
+        }
+        // if the node value is undefined...
+        catch(e) {
+            //..then assign its value as an empty string to prevent the app from crashing
+            NBAarray[i] = '';
+        }
 	}
 
-
-
-
-	// grab the values of node 'n', and append the values to an array
-	function assignArray(d, n, r)
-	{
-
-        var j=(d.getElementsByTagName(r).length + 2);
-      	var NBAarray;
-
-        NBAarray = new Array();
-        for(var i = 0; i < j; i++)
-        {
-
-        	// since its possible that a title or description entry from the RSS feed
-        	// will be empty, or have 'bad' characters, we need to do some error handling
-
-        	try
-        	{
- 	   			NBAarray[i] = d.getElementsByTagName(n).item(i).firstChild.data;
-        	}
-        	// if the node value is undefined...
-        	catch(e)
-        	{
-        		//..then assign its value as an empty string to prevent the app from crashing
-            	NBAarray[i] = '';
-        	}
-
-    	}
-
-        return NBAarray;
-	}
+	return NBAarray;
+}
